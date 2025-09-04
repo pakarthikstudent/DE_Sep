04f93ae89b05c5ad5aede8067bc49e98f01ca471
pakarthikstudent <162731772+pakarthikstudent@users.noreply.github.com>
2025-09-04 18:36:22 +0530

    Create p2.py

---
+++ DAY4/PySpark_Examples/p2.py
@@ -0,0 +1,14 @@
+# start spark session
+from pyspark.sql import SparkSession
+spark = SparkSession.builder.appName("example1").getOrCreate()
+
+data = [("pA",101),("pB",102),("pC",103)]
+columns = ["pName","pID"]
+
+df = spark.createDataFrame(data,columns)
+df.show()
+##
+df = spark.read.csv("prod.csv",header=True,inferSchema=True)
+df.printSchema()
+df.show()
+df.show(3)
