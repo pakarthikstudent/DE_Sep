721e4619781f2654dc842edbf89fc78e3af71c9d
pakarthikstudent <162731772+pakarthikstudent@users.noreply.github.com>
2025-09-04 18:36:41 +0530

    Create p3.py

---
+++ DAY4/PySpark_Examples/p3.py
@@ -0,0 +1,19 @@
+# start spark session
+from pyspark.sql import SparkSession
+spark = SparkSession.builder.appName("example1").getOrCreate()
+
+data = [("pA",101),("pB",102),("pC",103)]
+columns = ["pName","pID"]
+
+df = spark.createDataFrame(data,columns)
+df.show()
+##
+df = spark.read.csv("prod.csv",header=True,inferSchema=True)
+df.printSchema()
+df.show()
+df.show(3)
+##
+# simple transformation
+df.select("productName").show() # fetch specific column
+df.filter(df.pcost >3000).show() # filter - rows
+df.groupBy("pid").count().show() # group by and aggregate
